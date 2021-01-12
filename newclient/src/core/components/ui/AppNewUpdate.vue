ee39e2efc3f173b8f14127403bd9ef6b2f4714c2
olgakup <olga.kupchevskaya.812@my.csun.edu>
2021-01-12 13:54:56 -0800

    devop: updated app new update ui, added core component to the notice new block update

--- newclient/src/core/components/ui/AppNewUpdate.vue
+++ newclient/src/core/components/ui/AppNewUpdate.vue
@@ -1,13 +1,18 @@
 <template>
-    <v-btn
-        v-if="display"
-        :class="['new-block-alert', 'text-capitalize', 'ma-0', $vuetify.breakpoint.name === 'sm' || $vuetify.breakpoint.name === 'xs' ? 'caption' : '']"
-        flat
-        @click="onReload"
-    >
-        {{ buttonText }}
-        <v-icon class="ml-2 secondary--text fas fa-sync small-global-icon-font"></v-icon>
-    </v-btn>
+    <div v-if="display">
+        <v-btn v-if="$vuetify.breakpoint.name !== 'xs'" class="new-alert text-capitalize ma-0 caption" flat @click="onReload">
+            {{ buttonText }}
+            <v-icon class="ml-2 secondary--text fas fa-sync small-global-icon-font"></v-icon>
+        </v-btn>
+        <v-tooltip v-else right color="white" top content-class="tooltip-border-update">
+            <template #activator="{ on }">
+                <v-btn class="new-alert new-alert-small ma-0 px-0 caption" flat icon @click="onReload" v-on="on">
+                    <v-icon class="secondary--text fas fa-sync small-global-icon-font"></v-icon>
+                </v-btn>
+            </template>
+            <span class="black--text caption text-capitalize"> {{ buttonText }}</span>
+        </v-tooltip>
+    </div>
 </template>
 
 <script lang="ts">
@@ -23,6 +28,7 @@ export default class AppNewUpdate extends Vue {
     @Prop({ type: String, default: '' }) text!: string
     @Prop({ type: Number, default: 0 }) updateCount!: number
     @Prop({ type: Boolean, default: false }) hideCount!: boolean
+
     /*
     ===================================================================================
       Computed:
@@ -51,9 +57,28 @@ export default class AppNewUpdate extends Vue {
 </script>
 
 <style scoped lang="css">
-.new-block-alert {
-    height: 44px;
+.new-alert {
+    height: 30px;
     border: solid 1px #ffb647;
     background-color: rgba(254, 217, 161, 0.25) !important;
+}
+.new-alert-small {
+    max-width: 30px !important;
+}
+.tooltip-border-update {
+    border: solid 1px #ffb647 !important;
+    padding: 5px 10px !important;
+    opacity: 1 !important;
+    box-shadow: none !important;
+    background-color: rgba(254, 217, 161, 0.25) !important;
+}
+.right {
+    float: right;
+    border: solid 1px #ffb647 !important;
+    padding: 5px 10px !important;
+    background-color: rgba(254, 217, 161, 0.25) !important;
+    margin-left: 5px;
+    display: flex;
+    z-index: 2;
 }
 </style>
--- newclient/src/modules/blocks/components/NoticeNewBlock.vue
+++ newclient/src/modules/blocks/components/NoticeNewBlock.vue
@@ -1,8 +1,5 @@
 <template>
-    <v-btn v-if="display" class="new-block-alert text-capitalize ma-0" flat @click="onReload">
-        {{ buttonText }}
-        <v-icon class="ml-2 secondary--text fas fa-sync small-global-icon-font"></v-icon>
-    </v-btn>
+    <app-new-update :text="buttonText" :update-count="valueNumber" :hide-count="true" @reload="onReload" />
 </template>
 
 <script lang="ts">
@@ -10,8 +7,13 @@ import { Mixins, Component, Prop, Watch } from 'vue-property-decorator'
 import { NewBlockSubscription } from '@app/modules/blocks/NewBlockSubscription/newBlockSubscription.mixin'
 import { newBlock } from '@app/modules/blocks/blocks.graphql'
 import { TranslateResult } from 'vue-i18n'
+import AppNewUpdate from '@app/core/components/ui/AppNewUpdate.vue'
 
-@Component
+@Component({
+    components: {
+        AppNewUpdate
+    }
+})
 export default class NoticeNewBlock extends Mixins(NewBlockSubscription) {
     /*
     ===================================================================================
@@ -25,12 +27,11 @@ export default class NoticeNewBlock extends Mixins(NewBlockSubscription) {
       Data:
     ===================================================================================
   */
-    display: boolean = false
     countTotal: number = 0
     newTxsCount: number = 0
     newUncles: number = 0
     validID = ['block', 'tx', 'pending', 'uncle']
-    valueString: number = 0
+    valueNumber: number = 0
 
     /*
     ===================================================================================
@@ -54,18 +55,15 @@ export default class NoticeNewBlock extends Mixins(NewBlockSubscription) {
     @Watch('newBlockNumber')
     onNewBlockNumberChange(newVal: number, oldVal: number): void {
         if (this.pageId === this.validID[0] && this.newBlockNumber) {
-            this.display = true
             this.countTotal++
-            this.valueString = this.countTotal
+            this.valueNumber = this.countTotal
         }
         if (this.pageId === this.validID[1] && this.newTxs && this.newTxs > 0) {
-            this.display = true
             this.newTxsCount += this.newTxs
-            this.valueString = this.newTxs
+            this.valueNumber = this.newTxs
         }
         if (this.pageId === this.validID[3] && this.newUncles > 0) {
-            this.display = true
-            this.valueString = this.newUncles
+            this.valueNumber = this.newUncles
         }
     }
     /*
@@ -78,7 +76,7 @@ export default class NoticeNewBlock extends Mixins(NewBlockSubscription) {
      */
     onReload() {
         this.$emit('reload')
-        this.display = false
+        this.valueNumber = 0
         this.countTotal = 0
         this.newTxsCount = 0
         this.newUncles = 0
@@ -89,19 +87,10 @@ export default class NoticeNewBlock extends Mixins(NewBlockSubscription) {
       Computed:
     ===================================================================================
   */
-
     get buttonText(): String {
         const plural = this.countTotal === 1 ? 1 : 2
         const message = `message.update.${this.pageId}`
-        return `${this.valueString} ${this.$tc(message, plural)}`
+        return `${this.valueNumber} ${this.$tc(message, plural)}`
     }
 }
 </script>
-
-<style scoped lang="css">
-.new-block-alert {
-    height: 44px;
-    border: solid 1px #ffb647;
-    background-color: rgba(254, 217, 161, 0.25) !important;
-}
-</style>
